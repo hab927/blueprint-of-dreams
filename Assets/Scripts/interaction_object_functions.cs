7c333f8f2d170fb0dced27651477b687b93ee215
Rye29 <167343946+Rye29@users.noreply.github.com>
2025-11-23 12:49:42 -0800

    created "idol" system
    -assign an interactable object with either the tags "item" or "placement" under the layer "interactable" (must also contain a kinematic rigidbody)
    -assign material "interact_outline" as a second material to the mesh renderer
    -add "Interaction_object_functions" to pedestals and items, you can now configure what happens under "On_Item_Grabbed" when an item is grabbed by the player and "On_Item_Placed" when a pedestal detects that something was placed on it
    -assign a "player_hand_manager" script to the player camera and a trigger collider, the collider will act as the range of interaction when "e" is pressed, 'hand_offset' tells the script where to place items in relation to the camera's local space if the interactable is an item

---
+++ Assets/Scripts/interaction_object_functions.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR;
+
+public class interaction_object_functions : MonoBehaviour
+{
+    public Material outline_factor;
+    public int OutlineID;
+    public UnityEvent On_Item_Grabbed;
+    public UnityEvent On_Item_Placed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //public Action interaction;
+
+    void Start()
+    {
+        OutlineID = Shader.PropertyToID("_size");
+        outline_factor = transform.GetComponent<MeshRenderer>().materials[1];
+        outline_factor.SetFloat(OutlineID, 1f);
+    }
+    public void pickupItem(Transform player_hand, Vector3 hand_position)
+    {
+        transform.SetParent(player_hand);
+        transform.localRotation = Quaternion.identity;
+        transform.localPosition = hand_position;
+        if(On_Item_Grabbed != null)
+        {
+            On_Item_Grabbed.Invoke();
+        }
+    }
+
+    public void placeItem(Transform item, Vector3 position_offset)
+    {
+        item.parent = transform;
+        item.localPosition = position_offset;
+        item.localRotation = quaternion.identity;
+        if (On_Item_Placed != null)
+        {
+            On_Item_Placed.Invoke();
+        }
+
+    }
+
+    public void lerpOutlineInHelper()
+    {
+        StartCoroutine(lerpOutline(1f, 1.05f, 0.2f));
+    }
+    
+    public void lerpOutlineOutHelper()
+    {
+        StartCoroutine(lerpOutline(1.05f, 1f, 0.2f));
+    }
+
+    public IEnumerator lerpOutline(float start_val, float end_val, float lerp_time)
+    {
+        float timer = 0f;
+        while (timer < lerp_time)
+        {
+            float lerp_value = Mathf.Lerp(start_val, end_val, timer / lerp_time);
+            outline_factor.SetFloat(OutlineID, lerp_value);
+            yield return new WaitForEndOfFrame();
+            timer += Time.deltaTime;
+        }
+        outline_factor.SetFloat(OutlineID, end_val);
+    }
+
+    public void Debug_Event_Test(string eventText)
+    {
+        UnityEngine.Debug.Log("this is a test, "+ eventText);
+    }
+}
---
+++ Assets/Scripts/player_hand_manager.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class player_hand_manager : MonoBehaviour
+{
+    [Header("interactable layers")]
+    [SerializeField] private LayerMask interactableLayers;
+    List<GameObject> interactables = new List<GameObject>();
+    [Header("item grab settings")]
+    public Vector3 hand_offset;
+    [Header("item grab references")]
+    public Transform grabbed_item;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if ((interactableLayers.value & (1 << other.gameObject.layer)) != 0)
+        {
+            interactables.Add(other.gameObject);
+            interaction_object_functions IOF = other.transform.GetComponent<interaction_object_functions>();
+            if(IOF != null)
+            {
+                IOF.lerpOutlineInHelper();
+            }
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if ((interactableLayers.value & (1 << other.gameObject.layer)) != 0)
+        {
+            interactables.Remove(other.gameObject);
+            interaction_object_functions IOF = other.transform.GetComponent<interaction_object_functions>();
+            if(IOF != null)
+            {
+                IOF.lerpOutlineOutHelper();
+            }
+        }
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && interactables.Count > 0)
+        {
+            //UnityEngine.Debug.Log("interacted with " + interactables[0] + "!");
+            interaction_object_functions object_behavior = interactables[0].GetComponent<interaction_object_functions>();
+
+            if(object_behavior != null)
+            {
+                switch (object_behavior.transform.tag)
+                {
+                    case "item":
+                        //UnityEngine.Debug.Log("picked up an item!");
+                        object_behavior.pickupItem(transform, hand_offset);
+                        grabbed_item = object_behavior.transform;
+                        break;
+                    case "placement":
+                        //UnityEngine.Debug.Log("object is a pedestal");
+                        if(grabbed_item != null)
+                        {
+                            //UnityEngine.Debug.Log("placed an item!");
+                            object_behavior.placeItem(grabbed_item, new Vector3(0, 0.5f, 0));
+                            grabbed_item = null;
+                            break;
+                        }
+                        else
+                        {
+                            object_behavior = object_behavior.transform.GetChild(0).GetComponent<interaction_object_functions>();
+                            if(object_behavior != null)
+                            {
+                                object_behavior.pickupItem(transform, hand_offset);
+                                grabbed_item = object_behavior.transform;
+                            }
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
