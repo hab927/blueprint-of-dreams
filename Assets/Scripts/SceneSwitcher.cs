aa8c22356ec2e1fa6782ec9ee2cd353dad51dc18
Ryan England <raenglandwork@gmail.com>
2025-12-03 15:18:46 -0800

    Added Scene Switcher and made the Play button load in the first level.

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public static SceneSwitcher instance = null;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void StartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Test 1");
+    }
+
+    public void LoadScene(string level)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(level);
+    }
+
+    public void ReturnToMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Main Menu");
+    }
+}
