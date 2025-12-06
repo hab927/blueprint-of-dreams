4c1527453dcb593854249e3e20d60a6cb9847531
Rye29 <167343946+Rye29@users.noreply.github.com>
2025-12-05 19:02:54 -0800

    finished pre cleaneup
    -idk I'm tired and need to work on other projects

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+    //yes, I made a script for this, we're out of time
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
